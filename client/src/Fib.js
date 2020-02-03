1e93f837fcbfbbb7e5e42387873b045ea997f2a2
Vinsensius Angelo <vafrcor2009@gmail.com>
2020-02-03 18:26:31 +0700

    update codes

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -52,11 +52,11 @@ class Fib extends Component{
   render(){
     return (
       <div>
-        <form onSubmit={ this.handleSubmit}>
+        <form onSubmit={ this.handleSubmit }>
           <label>Enter your index: </label>
           <input 
             type="text" 
-            value={ this.state } 
+            value={ this.state.index } 
             onChange={ event => this.setState({ index: event.target.value })}
           />
           <button type="submit">submit</button>
