d10586469eb666c97baaf979cc51133c4a383a1c
Vinsensius Angelo <vafrcor2007@yahoo.co.id>
2020-02-19 14:44:39 +0700

    update: - update some files

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator
+            Fib Calculator v.2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
